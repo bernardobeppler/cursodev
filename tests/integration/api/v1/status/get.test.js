f918d0c1ef6b49e9f3af363016f44b7e59960ac6
Bernardo Beppler <betobeppler@gmail.com>
2025-08-05 18:51:41 -0300

    finaliza o endpoint /status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
-test("GET to /api/v1/status should return 200", async () => {   
+test("GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status")
     expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+
+    const parseUpdateAt = new Date(responseBody.update_at).toISOString();
+    expect(responseBody.update_at).toEqual(parseUpdateAt);
+
+    const database = responseBody.dependencies.database;
+    expect(database.version).toBe("16.0");
+    expect(database.max_connections).toBe(100);
+    expect(database.opened_connections).toBe(1);
 });
